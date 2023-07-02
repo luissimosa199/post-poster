148380cb2e040319101f109a1eea7629ae656c11
Luis Simosa <simosa37@gmail.com>
2023-07-02 16:35:27 -0300

    fix: uncomment responseCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main>
       <Form setApiResponse={setApiResponse} />
 
-      {/* {apiResponse && <ResponseCard data={apiResponse.data} />} */}
+      {apiResponse && <ResponseCard data={apiResponse.data} />}
 
     </main>
   )
