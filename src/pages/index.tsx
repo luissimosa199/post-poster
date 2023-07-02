3bf8fb711868a824fd54a922db54005c95152e7c
Luis Simosa <simosa37@gmail.com>
2023-07-02 16:31:25 -0300

    fix: responseCard name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import Form from '@/components/Form'
 import { type ResponseData } from '@/types/responseData'
-import ResponseCard from '@/components/responseCard'
-
+import ResponseCard from '@/components/ResponseCard'
 
 export default function Home() {
 
